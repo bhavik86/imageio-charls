e148ee9cdbf6b36d896fc23b629915099bbf2770
Aaron Boxer <boxerab@gmail.com>
2013-11-01 19:45:14 -0400

    changes due to imageio-generic refactor

--- src/main/java/com/codecCentral/imageio/charls/JPLSCharlsImageReaderSpi.java
+++ src/main/java/com/codecCentral/imageio/charls/JPLSCharlsImageReaderSpi.java
@@ -6,31 +6,19 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 
 import javax.imageio.ImageReader;
-import javax.imageio.spi.IIORegistry;
-import javax.imageio.spi.ImageReaderSpi;
-import javax.imageio.spi.ImageReaderWriterSpi;
-import javax.imageio.spi.ServiceRegistry;
 import javax.imageio.stream.ImageInputStream;
 
-import org.codecCentral.imageio.generic.NativeUtilities;
-import org.codecCentral.imageio.generic.Utils;
+import org.codecCentral.imageio.generic.DecoderBase;
+import org.codecCentral.imageio.generic.GenericImageReaderSpi;
 
 
-public class JPLSCharlsImageReaderSpi extends ImageReaderSpi {
 
+public class JPLSCharlsImageReaderSpi extends GenericImageReaderSpi {
 
-	static List<String> libraries;
-
-	static {
-		libraries = Arrays.asList("CharLS", "CharLS_JNI");
-        NativeUtilities.loadLibraries(libraries);
-    }
-	
 	
     protected boolean registered = false;
     
@@ -91,36 +79,17 @@ public class JPLSCharlsImageReaderSpi extends ImageReaderSpi {
                 nativeImageMetadataFormatClassName,
                 extraImageMetadataFormatNames,
                 extraImageMetadataFormatClassNames);
+        
+    	libraries = Arrays.asList("CharLS", "CharLS_JNI");
+        _utilities.loadLibraries(libraries);
+        format = "jpeg-ls";
     }
+    
+	protected DecoderBase CreateDecoder()
+	{
+		return new CharlsDecoder();
+	}
 
-    /**
-     * This method checks if the provided input can be decoded from this SPI
-     */
-    public boolean canDecodeInput(Object input) throws IOException {
-    	if (input == null)
-    		return false;
-    	
-        boolean isDecodable = true;
-    	CharlsDecoder decoder = new CharlsDecoder();
-
-        // Retrieving the File source
-        if (input instanceof File) {
-            isDecodable =  decoder.canDecode(((File) input).getAbsolutePath());
-          
-        } else if (input instanceof byte[]) 
-        {
-           // source = (byte[])input;
-        } else if (input instanceof URL)
-        {
-            final URL tempURL = (URL) input;
-            if (tempURL.getProtocol().equalsIgnoreCase("file")) {
-                //source = Utils.urlToFile(tempURL);
-            }
-        } else
-            return false;
-
-        return isDecodable;
-    }
 
     /**
      * Returns an instance of the {@link JPLSCharlsImageReader}
@@ -135,55 +104,8 @@ public class JPLSCharlsImageReaderSpi extends ImageReaderSpi {
      * @see javax.imageio.spi.IIOServiceProvider#getDescription(java.util.Locale)
      */
     public String getDescription(Locale locale) {
-        return new StringBuffer("JP2K Image Reader, version ").append(version)
-                .toString();
+        return new StringBuffer("JPEG-LS Image Reader, version ").append(version).toString();
     }
 
-    /**
-     * Upon registration, this method ensures that this SPI is listed at the top
-     * of the ImageReaderSpi items, so that it will be invoked before the
-     * default ImageReaderSpi
-     * 
-     * @param registry
-     *                ServiceRegistry where this object has been registered.
-     * @param category
-     *                a Class object indicating the registry category under
-     *                which this object has been registered.
-     */
-    @SuppressWarnings("unchecked")
-	public synchronized void onRegistration(ServiceRegistry registry, Class category) {
-        super.onRegistration(registry, category);
-        if (registered) {
-            return;
-        }
-
-        registered = true;
-        
-        
-        if (!NativeUtilities.areLibrariesAvailable(libraries)) {
-            final IIORegistry iioRegistry = (IIORegistry) registry;
-            final Class<ImageReaderSpi> spiClass = ImageReaderSpi.class;
-            final Iterator<ImageReaderSpi> iter = iioRegistry.getServiceProviders(spiClass,true);
-            while (iter.hasNext()) {
-                final ImageReaderSpi provider = (ImageReaderSpi) iter.next();
-                if (provider instanceof JPLSCharlsImageReaderSpi) {
-                    registry.deregisterServiceProvider(provider);
-                }
-            }
-            return;
-        }
-        
-        
-        final List<ImageReaderWriterSpi> readers = Utils.getJDKImageReaderWriterSPI(registry,"jpeg", true);
-        for (ImageReaderWriterSpi elem:readers) {
-        	if (elem instanceof ImageReaderSpi){
-	            final ImageReaderSpi spi = (ImageReaderSpi) elem;;
-	            if (spi == this)
-	                continue;
-	            registry.deregisterServiceProvider(spi);
-	            registry.setOrdering(category, this, spi);
-        	}
-
-        }
-    }
+  
 }
